abc4e2aab7b4b421ceabad18e977e718ff7fcc37
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-12 16:19:54 +0200

    home.blade: add card structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,17 @@
 
 @section('name')
     <section>
-      
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div class="card">
+              <div class="card-body">
+                <img src="" alt="">
+                <h4></h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
 @endsection
