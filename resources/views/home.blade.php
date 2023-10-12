020bab3494c108293d44e988e0109c1f426aafde
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-10-12 23:03:15 +0200

    edit card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,26 @@
 
 @section('main-content')
     <section>
-      <div class="container mt-5">
-        <div class="row">
+      <div class="container pt-5 pb-5">
+        <div class="row g-3">
 
           @foreach ($comic_books as $index => $comic_book)
           
           <div class="col-2">
             <div class="card">
-              <div class="card-body">
+              <div class="card-img">
                 <img src="{{ $comic_book['thumb'] }}" alt="" class="img-fluid">
+              </div>
+              <div class="card-body">
                 <h6>{{ $comic_book['title'] }}</h6>
               </div>
             </div>
           </div>
 
           @endforeach
-
+        </div>
+        <div class="d-flex justify-content-center" >
+          <button>LOAD MORE</button>
         </div>
       </div>
     </section>
